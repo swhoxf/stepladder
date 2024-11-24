735ddc1ccfda3f1d265050b3b60f05465e757ddc
swhoxf <108375735+swhoxf@users.noreply.github.com>
2024-11-24 17:52:47 -0500

    fixed typo

--- src/components/SignUpSection.js
+++ src/components/SignUpSection.js
@@ -8,7 +8,7 @@ const SignUpSection = () => {
   return (
     <section className="flex max-w-6xl mx-auto my-20 justify-between items-end">
       <div className="max-w-2xl">
-        <h2 className="my-4">Read to take the next step?</h2>
+        <h2 className="my-4">Ready to take the next step?</h2>
         <p className="my-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam et voluptate rerum eaque adipisci soluta placeat ad temporibus, laborum recusandae quas!
         </p>
