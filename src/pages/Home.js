ade25ef2ad1264c02519dd08eda70373154b3281
swhoxf <108375735+swhoxf@users.noreply.github.com>
2024-11-24 17:52:03 -0500

    moved hero section to separate component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import Button from '../components/Button.js'
 import ClientReview from '../components/ClientReview.js'
 import Card from '../components/Card.js'
+import HeroSection from '../components/HeroSection.js'
 import SignUpSection from '../components/SignUpSection.js'
 
 // import images
@@ -17,22 +18,7 @@ import { Apple } from 'lucide-react';
 const Home = () => {
   return (
     <>
-      <div className="hero mx-auto max-w-7xl flex gap-8 pt-20 justify-center my-20">
-        <div className="my-20 min-w-96 max-w-64 w-56">
-          <h1 className="my-4">
-            Financial stability is <span className="font-black italic relative before:absolute before:bg-orange-400 before:h-3 before:w-full before:bottom-1 before:right-2 before:z-[-1]">within reach</span>
-          </h1>
-          <p className="my-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam et
-            voluptate rerum eaque adipisci soluta placeat ad temporibus, laborum
-            recusandae quas!
-          </p>
-          <Button label="Take your first step" />
-        </div>
-        <div className="w-100">
-          <img src={heroImage} alt="man looking at keyboard"/>
-        </div>
-      </div>
+      <HeroSection />
       
       <section className="max-w-xl text-center my-20 mx-auto">
         <h2 className="m-4">Small fiancial changes can make a <span className="font-black italic">big difference</span></h2>
