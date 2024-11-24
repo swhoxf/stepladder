8cc712600591df2f0034f63d2653c2c618b67d1e
swhoxf <108375735+swhoxf@users.noreply.github.com>
2024-11-24 18:07:13 -0500

    added decorative quotation mark svg

--- src/components/ClientReview.js
+++ src/components/ClientReview.js
@@ -1,15 +1,21 @@
 // import utilities
 import React from "react";
 
+// import images
+import quotationMarks from '../assets/decorative/quotation-marks.svg'
+
 const ClientReview = (props) => {
   return (
-    <div className="border border-stone-200">
-      <p className="m-4">
-        { props.message }
-      </p>
-      <p className="m-4 text-sm text-stone-500">
-        { props.name }
-      </p>
+    <div className="border border-stone-200 flex gap-4 items-start p-6">
+      <img src={ quotationMarks } alt="quotation marks" />
+      <div>
+        <p className="mb-4">
+          { props.message }
+        </p>
+        <p className="my-b text-sm text-stone-500">
+          { props.name }
+        </p>
+      </div>
     </div>
   )
 }
