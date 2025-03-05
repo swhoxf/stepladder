92a9db64717c055473c59b0413ce3e203ccc37b2
swhoxf <108375735+swhoxf@users.noreply.github.com>
2025-03-04 19:50:25 -0500

    refactored card component to accept image and body text props

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,14 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className="bg-stone-100 p-4">
-      <p>{ props.header }</p>
-      <p className="text-sm text-stone-500">{ props.subheader }</p>
+    <div className="bg-stone-100">
+      { props.imgSrc && <img src={ props.imgSrc } alt={ props.imgAlt } /> }
+      <div className="p-4">
+        <p className="text-lg my-1">{ props.header }</p>
+        { props.subheader && <p className="text-sm text-stone-500">{ props.subheader }</p> }
+        { props.body && <p className="text-xs">{ props.body }</p> }
+        { props.caption && <p className="text-xs italic">{ props.caption }</p> }
+      </div>
     </div>
   )
 }
