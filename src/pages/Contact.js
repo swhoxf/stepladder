73eb801a451661cdcc52f55bfacae175a80c5963
swhoxf <108375735+swhoxf@users.noreply.github.com>
2025-03-04 20:34:17 -0500

    started working on contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,25 +1,48 @@
-import logo from '../assets/logo.svg';
-import '../App.css';
+// import utilities
+import React, { useState } from 'react'
 
-function Contact() {
+const Contact = () => {
+  const [name, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [nameError, setNameError] = useState('')
+  const [emailError, setEmailError] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  function handleNameChange(e) {
+    setFirstName(e.target.value)
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Contact</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form class="mx-auto grid w-96" onSubmit={ handleSubmit }>
+        <label for="name" class="block text-sm/6 font-medium text-gray-900">Name:</label>
+        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+          <input type="text" name="name" value={ name } 
+            onChange={ handleNameChange }
+            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+        </div>
+        { nameError && <div>{ nameError }</div>}
+
+        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email:</label>
+        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+          <input type="email" name="email" value={ email } onChange={ handleEmailChange } 
+            class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+          { nameError && <div>{ nameError }</div>}
+        </div>
+
+        <label for="message" class="block text-sm/6 font-medium text-gray-900">Message:</label>
+        <textarea class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600"></textarea>
+        <input type="submit" value="Submit" 
+          class="bg-green-300 px-8 py-4 my-4" />
+      </form>
+    </>
   );
 }
 
